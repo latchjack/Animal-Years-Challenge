3d1c41b6250805afb4a399885ed649c4277faeac
latch jack <latch@latchs-MacBook-Pro.local>
2019-04-04 00:13:55 +0100

    Initial commit

---
+++ script.js
@@ -0,0 +1,12 @@
+const catYears = 15;
+const dogYears = 15;
+// const userYears = prompt('how old are you?');
+const numby = document.querySelector(input);
+
+function catNum() {
+  var totalYears = userYears * catYears;
+  return totalYears;
+}
+
+console.log(totalYears);
+
