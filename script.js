12afcdf710f2a00b1537d141798b41f70725f7d0
latch jack <latch@latchs-MacBook-Pro.local>
2019-04-04 20:49:46 +0100

    added function
    added function to test out input event listener

--- script.js
+++ script.js
@@ -1,12 +1,15 @@
 const catYears = 15;
 const dogYears = 15;
+const texty = document.querySelector('#replaceMe');
 // const userYears = prompt('how old are you?');
-const numby = document.querySelector(input);
+// const numby = document.querySelector(input);
 
-function catNum() {
-  var totalYears = userYears * catYears;
-  return totalYears;
-}
+// function catNum() {
+//   var totalYears = userYears * catYears;
+//   return totalYears;
+// }
 
-console.log(totalYears);
 
+converter.addEventListener("input", function(e) {
+  alert(this.value);
+});
