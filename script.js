fd41f2a2fa5d4b76dc2fb2498181fb86b10f2584
latch jack <latch@latchs-MacBook-Pro.local>
2019-04-15 21:15:57 +0100

    redo

--- script.js
+++ script.js
@@ -3,8 +3,7 @@ const userYears = document.querySelector('#inYears');
 const numby = document.getElementById('userYears') * catYears;
 
 const texty = document.querySelector('#replaceMe');
-// const userYears = document.getElementById('converter');
-// const solveIt = document.getElementById(theButton).onclick = calcuLate();
+
 
 function calcuLate(inYears) {
   let combination = userYears * catYears;
@@ -12,13 +11,3 @@ function calcuLate(inYears) {
   console.log(numby());
 }
 
-
-// function catNum() {
-//   var totalYears = userYears * catYears;
-//   return totalYears;
-// }
-
-
-// converter.addEventListener("input", function(e) {
-//   texty.textContent = numby;
-// });
