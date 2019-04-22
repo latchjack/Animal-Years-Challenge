18d8e69e53bea5677dd6eb62ad012806cf8246e8
latch jack <latch@latchs-MacBook-Pro.local>
2019-04-22 22:21:02 +0100

    input changes
    trying to fix the grab integer from the input box. still broken. need to come back to this...

---
+++ animal.js
@@ -0,0 +1,15 @@
+const animalYears = 15;
+
+// This grabs the input box
+var theButton = document.getElementById(theButton);
+
+theButton.addEventListener('click',);
+
+// The below code works out the cat years x user years
+// I need to make the line below work when the button is
+// clicked. I need to assign it to a click event on the button.
+var userYears = parseInt(document.getElementById(inYears));
+
+var newYears = animalYears * userYears;
+
+console.log(newYears);
