fe3c97d49018d859d5a3430ef9f1ca022b2de200
latch jack <latch@latchs-MacBook-Pro.local>
2019-04-28 08:58:51 +0100

    updates

--- script.js
+++ script.js
@@ -1,10 +1,18 @@
 var userAge = prompt("how old are you?");
-// const userAgeOriginal = userAge;
+var userAgeOriginal = userAge;
+
+const texty = document.querySelector('#replace');
+const numButton = document.querySelector('#randomButton');
+
+numButton.addEventListener('click', function(e) {
+  texty.textContent = catYears();
+});
+
 
 function catYears() {
-  userAge =+ 15;
-  userAge =+ 9;
-  // userAge * userAgeOriginal;
+  userAge += 15;
+  userAge += 9;
+  userAge * userAgeOriginal;
 }
 
 console.log(catYears(userAge));
