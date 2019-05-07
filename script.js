28e8fd478c7e3c58c501bec15a283f31a5e597dd
latch jack <latch@latchs-MacBook-Pro.local>
2019-05-07 23:37:59 +0100

    fixing the function
    replaced the incorrect function with working function. Math still needs work though

--- script.js
+++ script.js
@@ -2,16 +2,32 @@ var firstYear = 15;
 var secondYear = 9;
 var onwardYear = 4;
 
-var usersAge = prompt('how old are you?');
+var usersAge;
+// var usersAge = prompt('how old are you?');
 
-if(usersAge < firstYear) {
-  console.log('your cat age is 0');
-} if else (usersAge > firstYear) {
-  console.log('your cat age is 15');
-} if else (usersAge > secondYear) {
-  console.log('your cat age is 24');
-} if else (usersAge > onwardYear) {
-  console.log('your cat age is 28');
-} else (usersAge > 28) {
-  usersAge += 4
+
+
+console.log(usersAge);
+
+const butt = document.querySelector('#submitButton')
+
+butt.addEventListener('click', function(e) {
+  var usersAge = prompt('how old are you?');
+
+  if(usersAge = firstYear) {
+    console.log('your cat age is 0');
+  } else if (usersAge > firstYear) {
+    console.log('your cat age is 15');
+  } else if (usersAge > secondYear) {
+    console.log('your cat age is 24');
+  } else if (usersAge > onwardYear) {
+    console.log('your cat age is 28');
+  } else (usersAge > 28); {
+    usersAge += 4;
+    console.log(usersAge);
+  }
+});
+
+function convertAge() {
+
 }
