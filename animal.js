a95040f0a06fc811b709d9590abb7c73ad9b2168
latch jack <latch@latchs-MacBook-Pro.local>
2019-05-09 22:26:54 +0100

    Update animal.js

--- animal.js
+++ animal.js
@@ -1,15 +1,3 @@
-const texty = document.querySelector('#replace');
-
-const butt = document.querySelector('#submitButton');
-
-butt.addEventListener('click', function(e) {
-  texty.textContent = Hi;
-});
-
-
-
-
-
 // New code //
 
 const grabNumber = document.querySelector('#usersAge');
@@ -30,18 +18,18 @@ let secondYear = 9;
 
 let followingYear = 4;
 
- 
+ // function below works but maths for numbers above 3 is incorrect, will come back and fix that at a later time. NEED to work out how to add 4 for each year older the user is after 3
 
 function convertAge(usersAge) {
 if (usersAge < 0) {
   return "your cat age is 0";
 } else if (usersAge === 1) {
   return "your cat age is 15";
-} else if (usersAge >= 2) {
+} else if (usersAge === 2) {
   return "your cat age is 24"
 } else if (usersAge > 2) {
   return "your cat age is 28"
-} else (usersAge >= 3) 
-  return "your cat age is 32"
+} else 
+  return "your cat age is 0"
 }
   
